ddce9560a312c22e1749b629641003bb7e3538ee
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2022-04-03 13:40:48 +0100

    Texas Holdem Hand Ranker

---
+++ src/main/scala/texasholdem/PokerHandRanker.scala
@@ -0,0 +1,21 @@
+package texasholdem
+
+object PokerHandRanker {
+
+
+  def rankHand(player:Player, table: Table): Int = {
+
+  }
+
+}
+
+
+// Improvements
+// Show current best combination between turns/rounds
+
+// Edge cases:
+// Same hand => Find higher hand with high card / suite
+// Ace High/Low for Straight / Straight flush
+
+
+// Highest to lowest: A, K, Q, J, 10, 9, 8, 7, 6, 5, 4, 3 and 2
