eb5c5b2a9014c3c5a1f1103c4fbc8919e58c0e78
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2022-04-04 17:41:54 +0100

    WIP Ranking poker hands and unit tests

--- src/main/scala/texasholdem/PokerHandRanker.scala
+++ src/main/scala/texasholdem/PokerHandRanker.scala
@@ -7,15 +7,70 @@ object PokerHandRanker {
     1
   }
 
-  def rankHand(player: Player, table: Table): PokerHand = {
-    val allCards = player.hand ++ table.cards
+  // Return the type of hand and its cards
+  def rankHand(player: Player, table: Table): (PokerHand, Seq[Card]) = {
+    val allCardsSorted: Seq[Card] = (player.hand ++ table.cards).sortBy(_.number)
+    if (allCardsSorted.length != 7) {
+      throw new RuntimeException("There should be only 7 cards total in hand and table")
+    }
+    val straight = hasStraight(allCardsSorted)
 
-    HighCard
+    println(allCardsSorted)
+    println(straight)
+
+    (HighCard, allCardsSorted.take(1))
   }
 
+  // Return boolean and the matched cards. Input is sorted cards
+  // Edge cases: High / low Ace
+  // https://stackoverflow.com/questions/6985148/scala-detecting-a-straight-in-a-5-card-poker-hand-using-pattern-matching
+  private def hasStraight(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    var count = 0
+    (false, Seq())
+  }
 
+  private def hasFlush(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    // Find the suit with 5 or more cards
+    //    cards.count(card => card.suit ==)
+    // Sort card by high cards (Ace, mod 13?)
+    ???
+  }
+
+  private def hasFullHouse(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    ???
+  }
+
+  private def hasThreeOfAKind(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    ???
+  }
+
+  private def hasTwoPairs(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    ???
+  }
+
+  private def hasOnePair(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    ???
+  }
+
+  private def hasHighCard(cards: Seq[Card]): (Boolean, Seq[Card]) = {
+    if (cards.head.number == 1) {
+      (true, Seq(cards.head))
+    } else {
+      (true, Seq(cards.last))
+    }
+  }
 }
 
+// Check Straight Flush by checking Straight + Flush
+// Check Four of a kind
+// Check Full house by checking Three of a kind + (Different) Pair
+// Check Straight from previous result
+// Check Flush from previous result
+// Check Check Three of a kind from previous result
+// Check Two pairs from previous result
+// Check One pair
+// Check High card
+// return immediately if you find a high card -> Do not have to go down all the conditional path
 
 // Improvements
 // Show current best combination between turns/rounds
--- src/test/scala/texasholdem/PokerHandRankerTest.scala
+++ src/test/scala/texasholdem/PokerHandRankerTest.scala
@@ -10,26 +10,40 @@ class PokerHandRankerTest extends AnyFlatSpec with Matchers {
     val player = Player(Seq(Card(13, Hearts), Card(12, Hearts)))
     val table = Table(Seq(Card(11, Hearts), Card(10, Hearts), Card(1, Hearts), Card(3, Diamonds), Card(3, Clubs)))
 
-    PokerHandRanker.rankHand(player, table) shouldEqual StraightFlush
+    PokerHandRanker.rankHand(player, table) shouldEqual
+      (StraightFlush, Seq(Card(13, Hearts), Card(12, Hearts), Card(11, Hearts), Card(10, Hearts), Card(1, Hearts)))
   }
 
   it should "rank straight flush - low ace" in {
     val player = Player(Seq(Card(1, Spades), Card(2, Spades)))
     val table = Table(Seq(Card(3, Spades), Card(4, Spades), Card(5, Spades), Card(7, Clubs), Card(9, Diamonds)))
 
-    PokerHandRanker.rankHand(player, table) shouldEqual StraightFlush
+    PokerHandRanker.rankHand(player, table) shouldEqual
+      (StraightFlush, Seq(Card(1, Spades), Card(2, Spades), Card(3, Spades), Card(4, Spades), Card(5, Spades)))
   }
 
   it should "rank four of a kind" in {
+    val player = Player(Seq(Card(1, Spades), Card(2, Spades)))
+    val table = Table(Seq(Card(2, Diamonds), Card(4, Spades), Card(2, Hearts), Card(2, Clubs), Card(9, Diamonds)))
 
+    PokerHandRanker.rankHand(player, table) shouldEqual
+      (FourOfAKind, Seq(Card(2, Spades), Card(2, Hearts), Card(2, Diamonds), Card(2, Clubs)))
   }
 
   it should "rank full house" in {
+    val player = Player(Seq(Card(13, Spades), Card(13, Hearts)))
+    val table = Table(Seq(Card(3, Clubs), Card(3, Spades), Card(3, Diamonds), Card(4, Clubs), Card(7, Hearts)))
 
+    PokerHandRanker.rankHand(player, table) shouldEqual
+      (FullHouse, Seq(Card(13, Spades), Card(13, Hearts), Card(3, Clubs), Card(3, Spades), Card(3, Diamonds)))
   }
 
   it should "rank flush" in {
+    val player = Player(Seq(Card(13, Spades), Card(13, Hearts)))
+    val table = Table(Seq(Card(3, Clubs), Card(3, Spades), Card(3, Diamonds), Card(4, Clubs), Card(7, Hearts)))
 
+    PokerHandRanker.rankHand(player, table) shouldEqual
+      (FullHouse, Seq(Card(13, Spades), Card(13, Hearts), Card(3, Clubs), Card(3, Spades), Card(3, Diamonds)))
   }
 
   it should "rank straight" in {
@@ -49,6 +63,10 @@ class PokerHandRankerTest extends AnyFlatSpec with Matchers {
   }
 
   it should "rank high card" in {
+    val player = Player(Seq(Card(1, Spades), Card(2, Spades)))
+    val table = Table(Seq(Card(3, Spades), Card(4, Spades), Card(5, Spades), Card(7, Clubs), Card(9, Diamonds)))
+
+    PokerHandRanker.rankHand(player, table) shouldEqual (HighCard)
 
   }
 
