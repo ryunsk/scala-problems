798992f4890cb4ecc5e1131d56acdc057d0488be
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2022-05-09 13:40:48 +0100

    Initial sentence construction

--- src/main/scala/markshaney/MarkShaney.scala
+++ src/main/scala/markshaney/MarkShaney.scala
@@ -1,22 +1,36 @@
 package markshaney
 
 import scala.io.Source
+import scala.util.Random
 
 object MarkShaney {
 
   def readInput(filePath: String): Seq[String] = {
     val lines = Source.fromResource(filePath).getLines().filter(_.nonEmpty)
-    lines.toList
+    lines.toList.flatMap(_.split(" ").toList)
   }
 
-  def generateNextWord(input: Seq[String]): String = {
-    ???
+  def wordsToTriplets(words: Seq[String]): Seq[Seq[String]] = {
+    words.sliding(3, 3).toSeq
+  }
+
+  def generateNextWord(input: Seq[Seq[String]]): String = {
+    val randomIndex = Random.nextInt(input.length)
+    val words = " " + input(randomIndex)(0) + " " + input(randomIndex)(1) + " " + input(randomIndex)(2)
+    words
   }
 
   def main(args: Array[String]): Unit = {
-    //    val text = readInput("mark-shaney/input-string.txt")
-    val text = readInput("mark-shaney/input-string-test.txt")
+    val text = readInput("mark-shaney/input-string.txt")
+    //    val text = readInput("mark-shaney/input-string-test.txt")
+    val triplets = wordsToTriplets(text)
 
+    var i = 0
+    while (i < 10) {
+      val r = generateNextWord(triplets)
+      print(r)
+      i += 1
+    }
   }
 
 }
