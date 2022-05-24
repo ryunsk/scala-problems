965bc016833fbcf9ce1d55167ba04a654db7c0d4
Ryun Shub Kim <ryun.shub.kim@gmail.com>
2022-05-24 14:53:09 +0100

    Input parser

--- src/main/scala/marsrover/InputParser.scala
+++ src/main/scala/marsrover/InputParser.scala
@@ -4,7 +4,11 @@ import scala.io.Source
 
 object InputParser {
   private val lines = readInput("mars-rover/input.txt")
-  val gridDimensions = lines
+  val gridDimensions: Seq[String] = lines(0).split(" ")
+  val rover1Position: Seq[String] = lines(1).split(" ")
+  val rover1Instructions: String = lines(2)
+  val rover2Position: Seq[String] = lines(3).split(" ")
+  val rover2Instructions: String = lines(4)
 
   private def readInput(filePath: String): Seq[String] = {
     val lines = Source.fromResource(filePath).getLines()
--- src/test/scala/marsrover/InputParserTest.scala
+++ src/test/scala/marsrover/InputParserTest.scala
@@ -5,18 +5,18 @@ import org.scalatest.matchers.should.Matchers
 
 class InputParserTest extends AnyFlatSpec with Matchers {
   it should "get grid dimensions" in {
-    println(InputParser.gridDimensions)
+    InputParser.gridDimensions shouldEqual Seq("5", "5")
   }
   it should "rover 1 location" in {
-
+    InputParser.rover1Position shouldEqual Seq("1", "2", "N")
   }
   it should "rover 1 instructions" in {
-
+    InputParser.rover1Instructions shouldEqual "LMLMLMLMM"
   }
   it should "rover 2 location" in {
-
+    InputParser.rover2Position shouldEqual Seq("3", "3", "E")
   }
   it should "rover 2 instructions" in {
-
+    InputParser.rover2Instructions shouldEqual "MMRMMRMRRM"
   }
 }
